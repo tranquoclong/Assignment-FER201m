dcf18b26b529b328fd9b0f03ed5d67c9172882e0
tranquoclong <tranquoclong1310@gmail.com>
2022-06-01 20:05:18 +0700

    ADD Assignment 2/3

---
+++ assets/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready((_) => {
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#mycarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+      $("#mycarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
+  });
+
+  // Login modal
+  $("#loginLink").click(function () {
+    $("#loginModal").modal();
+  });
+
+  // Reserve Table Modal
+  $("#reservationButton").click(function () {
+    $("#reservationModal").modal();
+  });
+});
